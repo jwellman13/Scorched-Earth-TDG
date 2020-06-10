46e71622c80b631936f8c2b40700e3d72a4db6a7
jwellman13 <josephewellman@gmail.com>
2020-06-10 10:16:26 -0700

    Initial Commit
    Migrated assets from original project folder to repo

---
+++ Scorched-Earth/Assets/Scripts/ClickToDestroy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ClickToDestroy : MonoBehaviour
+{
+
+    [SerializeField] Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse clicked!");
+            Vector3 mousePos3d = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = new Vector2(mousePos3d.x, mousePos3d.y);
+
+            RaycastHit2D hitInfo = Physics2D.Raycast(mousePos, Vector2.zero);
+            if(hitInfo.collider != null)
+            {
+                Debug.Log(hitInfo.collider.name);
+                Collider2D[] hitObjects = Physics2D.OverlapCircleAll(hitInfo.point, 1.5f);
+                foreach(Collider2D obj in hitObjects)
+                {
+                    Destroy(obj.gameObject);
+                }
+                
+            }
+        }
+    }
+
+    
+}
---
+++ Scorched-Earth/Assets/Scripts/Terrain/MapGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] Tile tile;
+    [SerializeField] Tile transitionTile;
+    [SerializeField] Tile topTile;
+    private int mapWidth = 440;
+    private int mapHeight = 150;
+    private int maxHeight;
+
+    void Start()
+    {
+        maxHeight = Random.Range(10, mapHeight);
+        GenerateTerrain();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateTerrain()
+    {
+        for (int i = 0; i < mapWidth; i++)
+        {
+           
+            int columnMaxHeight = maxHeight - Random.Range(-1, 2);
+            maxHeight = columnMaxHeight;
+            for (int j = 0; j < columnMaxHeight; j++)
+            {
+                float xOffset = .064f;
+                float yOffset = .064f;
+                Vector3 spawnPos = new Vector3(transform.position.x + (xOffset * i), transform.position.y + (yOffset * j), transform.position.z);
+                if(j == columnMaxHeight -1)
+                {
+                    GameObject.Instantiate(topTile, spawnPos, transform.rotation, transform);
+                }
+                else if (j == columnMaxHeight - 2)
+                {
+                    GameObject.Instantiate(transitionTile, spawnPos, transform.rotation, transform);
+                }
+                else
+                {
+                    GameObject.Instantiate(tile, spawnPos, transform.rotation, transform);
+                }
+            }
+            
+        }
+        
+        
+    }
+}
